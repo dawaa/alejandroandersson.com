acc4f5711721bd81e49f82cf4d886b8a708cc548
Alejandro Andersson <alejandro@thelad.io>
2020-06-23 10:35:52 +0200

    Add build.sh helper script
    I want links without the `.html` extension in them so I'm omitting that
    part of the filename except for `index.html` and `404.html`. Because I'm
    omitting `.html` I'm also adding the `_includes.head.html` to provide
    one additional meta tag regarding `Content-Type`.. otherwise it's a
    direct copy of `minima/_includes/head.html`.
    
    Also ignore `tags` as it's from my Vim setup generating tags and is not
    part of this project and even less so the deployment.

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+bundle exec jekyll build \
+    && find _site \
+        -name '*.html' \
+        -not -name 'index.html' \
+        -not -name '404.html' \
+        -exec sh -c 'mv $1 ${1%.*}' sh {} \;
